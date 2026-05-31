8d41edc5d35765e11b1aafa7ea5547c87a49575f
Julien Brizou <julienbrizou@gmail.com>
2026-05-31 15:15:23 +0200

    Maj Games display

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ let zTop = 1000;
 
 /* ─────────────── Jeux list ─────────────── */
 const gameSlugs = [
-  'Resident_Evil_2', 'Ico', 'Zelda_Skyward_Sword',
-  "Zelda_Majora's_Mask", 'Zelda_Ocarine_of_Time', 'Shenmue','Xanadu_Next',
+  'Resident_Evil_2', 'Ico',
 ];
 
 /* ──────────────────────────────────────────
